d6052dee4279c0248283f922ab3916fc3dc211ff
Desirée Espinosa <116221093+DesireeEspinosa@users.noreply.github.com>
2023-06-11 02:48:31 -0600

    Update Alimento.cpp

--- Proyecto/Alimento.cpp
+++ Proyecto/Alimento.cpp
@@ -7,7 +7,7 @@
 // Constructor que lee de un archivo
 Alimento::Alimento(ifstream& file) {
     // Lee el nombre hasta la primera coma
-    std::getline(file, nombre, ',');
+    getline(file, nombre, ',');
 
     // Lee las calorías hasta el final de la línea
     string strCalorias;
